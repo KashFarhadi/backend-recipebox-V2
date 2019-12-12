c015c942d71f93ffe5e3518f0404be3007c7bfb3
KashF <Kashfarhadi@gmail.com>
2019-12-11 20:36:25 -0500

    cloned from cmjoy136/recipebox

---
+++ recipebox/models.py
@@ -0,0 +1,34 @@
+"""
+Author
+    Name
+    Bio
+
+Recipe
+    Author
+    Title
+    Description
+    Time Required
+    Instructions 
+
+"""
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    time_required = models.CharField(max_length=50)
+    instructions = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} - {self.author.name}"
---
+++ recipebox/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from recipebox.models import Author, Recipe
+from recipebox.forms import AuthorAddForm, RecipeAddForm, LoginForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    html = "index.html"
+    recipes = Recipe.objects.all()
+
+
+    return render(request, html, {'data': recipes})
+
+def author(request, author_id):
+    html = "author.html"
+    author = Author.objects.filter(id=author_id).first()
+    recipes = Recipe.objects.filter(author=author_id)
+
+    return render(request, html, {"data": author, 'recipes': recipes})
+
+def recipe(request, recipe_id):
+    html = 'generic_form.html'
+    recipe = Recipe.objects.filter(id=recipe_id).first()
+
+    return render(request, html, {"data": recipe})
+
+
+def authoraddview(request):
+    html = "generic_form.html"
+    form = AuthorAddForm()
+
+    if request.method == 'POST':
+        form = AuthorAddForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            Author.objects.create(
+                name = data['name'],
+                bio = data['bio']
+            )
+
+    return render(request, html, {'form': form})
+
+@login_required
+def recipeaddview(request):
+    html = "generic_form.html"
+    form = RecipeAddForm()
+    if request.method == "POST":
+        form  = RecipeAddForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            Recipe.objects.create(
+                author = data["author"],
+                title = data['title'],
+                description = data['description'],
+                time_required = data['time_required'],
+                instructions = data['instructions']
+            )
+            return HttpResponseRedirect(reverse('homepage'))
+    
+
+    return render(request, html, {'form': form})
+
+def login_view(request):
+    html = "generic_form.html"
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            if user := authenticate(
+                username=data['username'],
+                password=data['password']
+            ):
+                login(request, user)
+                return HttpResponseRedirect(
+                    request.GET.get('next', '/'))
+
+    return render(request, html, {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('homepage'))
+
+
+
